b4acd627cc3931b32812b622c3447c646e4c3a19
Tomoya Kuwashima <apple.941.s.j@gmail.com>
2022-12-26 15:08:27 +0000

    Fix import memo problems.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, memo } from "react";
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
 
-export const App = () => {
+export const App = memo(() => {
   console.log("Appレンダリング");
 
   const [num, setNum] = useState(0);
@@ -11,12 +11,17 @@ export const App = () => {
     setNum(num + 1);
   };
 
+  const onClickReset = () => {
+    setNum(0);
+  };
+
   return (
     <>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
-      <Child1 />
+      {/* Propsとして関数を設定 */}
+      <Child1 onClick={onClickReset} />
       <Child4 />
     </>
   );
-};
+});
